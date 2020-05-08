81b446d3d075f51f47911e8e9c54388422542ddc
xj.z <zzhjsan@gmail.com>
2020-05-08 18:12:34 +0800

    update

---
+++ lib/widget/app_screen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class AppScreen extends StatefulWidget {
+  @override
+  _AppScreenState createState() => _AppScreenState();
+}
+
+class _AppScreenState extends State<AppScreen> with TickerProviderStateMixin {
+  AnimationController animationController;
+
+  List<TabIconData> tabIconsList = TabIconData.tabIconsList;
+
+  Widget pageBody;
+
+  Future<bool> getData() async {
+    await Future<dynamic>.delayed(const Duration(milliseconds: 200));
+    return true;
+  }
+
+  @override
+  void initState() {
+    tabIconsList.forEach((TabIconData tab){
+      tab.isSelected = false;
+    });
+    tabIconsList[0].isSelected = true;
+
+    animationController = AnimationController(
+      duration: const Duration(milliseconds: 600), vsync: this
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder<bool>(
+        future: getData(),
+        builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CupertinoActivityIndicator(),
+            );
+          } else {
+            return Stack(
+              children:<Widget>[
+                pageBody,
+                bottomBar(),
+              ]
+            );
+          }
+        },
+      )
+    );
+  }
+
+  Widget bottomBar() {
+    return Column(
+      children:<Widget>[
+        const Expanded(
+          child: SizedBox()
+        ),
+        BottomBarView(
+          tabIconsList: tabIconsList,
+          addClick: () {},
+          changeIndex: (int index) {
+            if (index == 0) {
+              animationController.reverse().then<dynamic>((data) {
+                if (!mounted) {
+                  return;
+                }
+                setState(() {
+                  pageBody = HomePageScreen(animationController: animationController);
+                });
+              });
+            }
+          }
+        )
+      ]
+    );
+  }
+
+}
